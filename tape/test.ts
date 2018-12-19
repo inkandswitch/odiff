d29d40cc254d4a805487ec195cf573d2a11ed557
Jeff Peterson <jeff@yak.sh>
2018-12-18 16:11:43 -0800

    Force similarity

---
+++ tape/test.ts
@@ -0,0 +1,69 @@
+import tape from "tape"
+import odiff from "../odiff"
+
+
+tape("a simple change creates a simple diff", t => {
+
+  expectChanges(t,
+    { a: 1 },
+    { a: 2 },
+    [
+      {
+        type: "set",
+        path: ["a"],
+        val: 2
+      }]
+  )
+
+  expectChanges(t,
+    { a: 1 },
+    { },
+    [
+      {
+        type: "unset",
+        path: ["a"],
+      }]
+  )
+
+  expectChanges(t,
+    [1, 2],
+    [1, 2, 3],
+    [
+      {
+        type: "add",
+        path: [],
+        index: 2,
+        vals: [3]
+      }]
+  )
+
+  expectChanges(t,
+    {arr: [1, 2]},
+    {arr: [1, 2, 3]},
+    [
+      {
+        type: "add",
+        path: ["arr"],
+        index: 2,
+        vals: [3]
+      }]
+  )
+
+  expectChanges(t,
+    {title: "Hello", body: ""},
+    {title: "Hello there", body: ""},
+    [
+      {
+        type: "set",
+        path: ["title"],
+        val: "Hello there",
+      }]
+  )
+
+  t.end()
+})
+
+
+function expectChanges(t: any, a: any, b: any, changes: any) {
+  return t.deepEqual(odiff(a, b), changes)
+}
